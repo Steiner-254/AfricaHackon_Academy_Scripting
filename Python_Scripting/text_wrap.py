5d0b51d78bf5b8b024e465caa6dfd057b8bebd90
Steiner-254 <steineralvin3@gmail.com>
2025-12-18 16:35:01 +0300

    updates on the text_wrap script

--- Python_Scripting/text_wrap.py
+++ Python_Scripting/text_wrap.py
@@ -1,5 +1,5 @@
 import textwrap
 
-text = "Python is an amazing programming language that is widely used for web development, data science, automation, and more."
+text = "It is good to learn Python before Christmas btw because - Python is an amazing programming language that is widely used for software engineering and more."
 wrapped_text = textwrap.fill(text, width=50)
 print(wrapped_text)
